bab59f8094b27a99192173f6cf7214fde7799f43
lukedirago <lucasdirago@gmail.com>
2020-07-20 16:40:56 -0300

    Update pyscope.py

--- pyscope.py
+++ pyscope.py
@@ -61,7 +61,7 @@ class pyscope :
         text = font.render(now, True, white)
         textRect = text.get_rect()
         textRect.center = (X // 2, Y // 2)
-        while True:
+        for x in range(0,150):
             clock.tick()
             self.screen.fill(gray)
             gettime = datetime.now()
