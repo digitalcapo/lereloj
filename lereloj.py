9baa1483b9ee43aefb1e57d3b9cc88a387162f09
lukedirago <lucasdirago@gmail.com>
2020-07-21 23:16:56 -0300

    lereloj first commit
    - initialize main class for the pygame metric calendar
    - set videodriver for the raspberry pi

---
+++ lereloj.py
@@ -0,0 +1,92 @@
+import pygame
+import os
+import time
+from datetime import datetime
+import pytz.reference
+
+pc = False
+
+class lereloj:
+    screen = None;
+
+    def __init__(self):
+        "Initializes a new pygame using the framebuffer"
+        # Based on "Python GUI in Linux frame buffer"
+        # http://www.karoltomala.com/blog/?p=679
+        # Define color pallete for further use
+        self.white = (255,255,255)
+        self.black = (0,0,0)
+        self.gray = (125,125,125)
+
+        if not pc:
+            # quick and dirty way 
+            #
+            disp_no = os.getenv("DISPLAY")
+            if disp_no:
+                print("I'm running under X display = {0}".format(disp_no))
+            # Use fbcon as display driver
+            os.putenv('SDL_VIDEODRIVER', 'fbcon')
+            try:
+                pygame.display.init()
+            except pygame.error:
+                print('Pygame Display Init failed')
+
+            size = (pygame.display.Info().current_w,
+                    pygame.display.Info().current_h)
+
+            print("Framebuffer size: {0} x {1}".format(size[0], size[1]))
+            self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+            # Clear screen
+            self.screen.fill((0,0,0))
+            # Init font support
+            # Will replace later with ptext module
+            pygame.font.init()
+            # Render screen
+            pygame.display.update()
+        else:
+            pygame.display.init()
+            pygame.font.init()
+            self.screen = pygame.display.set_mode((640,360))
+            self.screen.fill((0,0,0))
+            pygame.display.update()
+
+
+    def decimalTime(self):
+        """
+        :return: Decimal time according to local time zone
+        :rtype: list
+        """
+        # Get local datetime from timezone
+        ltz = pytz.reference.LocalTimezone()
+        now = datetime.now(ltz)
+        # Calcuate time from midnight
+        midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        nsfm = (now - midnight).total_seconds()
+        # Remap normal seconds to metric seconds
+        metricSeconds = nsfm / 0.864
+
+        hours = int(metricSeconds / 10000)
+        minutes = int(metricSeconds / 100) % 100
+        seconds = int(metricSeconds % 100)
+        miliseconds = int((metricSeconds - int(metricSeconds)) * 1000)
+        metricTime = [hours,minutes,seconds,miliseconds]
+        return metricTime
+
+    def leClock(self):
+        clock = pygame.time.Clock()
+        fontFile = 'Digestive.otf'
+        font = pygame.font.Font(fontFile, 180)
+        for x in range(0,40000):
+            clock.tick()
+            self.screen.fill(self.black)
+            text = font.render(str(self.decimalTime()), True, self.white)
+            textRect = text.get_rect()
+            textRect.center = (640//2,360//2)
+            self.screen.blit(text,textRect)
+            pygame.display.update()
+    # def __del__(self):
+
+
+if __name__ == '__main__':
+    lereloj = lereloj()
+    lereloj.leClock()
